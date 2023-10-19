1d108ef82e522e434998dad5e6e54790d0099357
preciousNext <130280522+preciousNext@users.noreply.github.com>
2023-10-19 01:25:48 +0100

    Add files via upload

---
+++ day3/day3.js
@@ -0,0 +1,73 @@
+var workoutSelect = document.getElementById("workoutSelect");
+var durationInput = document.getElementById("durationInput");
+var startButton = document.getElementById("start");
+var pauseButton = document.getElementById("pause");
+var endButton = document.getElementById("end");
+var timer = document.getElementById("timer");
+var cong = document.getElementById("cong");
+
+let workoutInterval;
+let isPaused = false;
+let totalTime = 0;
+
+function startWorkout() {
+  const workoutName = workoutSelect.value;
+  const duration = parseInt(durationInput.value, 10);
+  if (!workoutName || isNaN(duration) || duration <= 0 || duration < 60) {
+    alert("Please select a workout and enter a valid duration");
+    return;
+  }
+
+  clearInterval(workoutInterval);
+  totalTime = duration;
+  workoutSelect.disabled = true;
+  durationInput.disabled = true;
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+  endButton.disabled = false;
+
+  workoutInterval = setInterval(function () {
+    if (!isPaused) {
+      totalTime--;
+      if (totalTime <= 0) {
+        endWorkout();
+      }
+      updateTimer();
+    }
+  }, 1000);
+  updateTimer();
+}
+
+function pauseWorkout() {
+  isPaused = !isPaused;
+  pauseButton.textContent = isPaused ? "Resume" : "Pause";
+}
+function endWorkout() {
+  clearInterval(workoutInterval);
+  workoutSelect.disabled = false;
+  durationInput.disabled = false;
+  startButton.disabled = false;
+  pauseButton.disabled = true;
+  endButton.disabled = true;
+  workoutSelect.value = "";
+  durationInput.value = "";
+  workoutSelect.focus();
+  timer.textContent = "Workout Complete!";
+}
+
+function updateTimer() {
+  const minutes = Math.floor(totalTime / 60);
+  const seconds = totalTime % 60;
+  timer.textContent = `${minutes.toString().padStart(2, "0")} : ${seconds
+    .toString()
+    .padStart(2, "0")}`;
+}
+startButton.addEventListener("click", function () {
+  startWorkout();
+});
+pauseButton.addEventListener("click", function () {
+  pauseWorkout();
+});
+endButton.addEventListener("click", function () {
+  endWorkout();
+});
